5a25c76dae576b02d832abdf56172f1c95de7100
Mikhail Petrachenka <77238687+aCrYoZPS@users.noreply.github.com>
2026-03-19 09:36:59 +0300

    lab3 done

--- lab3.py
+++ lab3.py
@@ -1,10 +1,12 @@
-from lab2 import LPProblem
-from lab1 import solve
-import numpy as np
 import math
 
+import numpy as np
 
-class BasisPlan:
+from lab2 import LPProblem
+from lab1 import solve
+
+
+class BasicSolution:
     def __init__(self, x: list, basis: list):
         # basis is ZERO INDEXED
         self.x = np.array(x, dtype=float)
@@ -15,41 +17,37 @@ class BasisPlan:
         print(f"B: {self.basis}")
 
 
-def simplex_main_phase(problem: LPProblem, basis_plan: BasisPlan) -> np.ndarray | None:
+def simplex_main_phase(problem: LPProblem, basic_solution: BasicSolution) -> np.ndarray | None:
     A_b_inv = None
     k = None
     c, A, b, offset = problem.canonicalize()
 
-    print(A)
-    A_b = A[:, basis_plan.basis]
+    A_b = A[:, basic_solution.basis]
     while True:
-        c_b = c[basis_plan.basis]
+        c_b = c[basic_solution.basis]
         if A_b_inv is None:
-            print(A_b)
             A_b_inv = np.linalg.inv(A_b)
         else:
-            A_b_inv = solve(A_b_inv, A[:, basis_plan.basis[k]], k + 1)
-        print(f"c_b: {c_b}")
-        print(f"A_b_inv: {A_b_inv}")
-        u = c_b * A_b_inv
-        delta = c - u * A
+            A_b_inv = solve(A_b_inv, A[:, basic_solution.basis[k]], k + 1)
+        u = c_b @ A_b_inv
+        delta = c - u @ A
         is_optimal = True
         j_0 = None
-        for item in delta:
+        for i, item in enumerate(delta):
             if item > 0:
                 is_optimal = False
-                if j_0 is None:
-                    j_0 = item
+                j_0 = i
+                break
 
         if is_optimal:
-            return basis_plan.x
+            return basic_solution.x
 
-        z = A_b_inv * A[:, j_0]
+        z = A_b_inv @ A[:, int(j_0)]
 
         theta: list[float] = []
         for i, z_i in enumerate(z):
-            if (z_i > 0):
-                theta.append(basis_plan.x[basis_plan.basis[i]]/z_i)
+            if z_i > 0:
+                theta.append(basic_solution.x[basic_solution.basis[i]] / z_i)
             else:
                 theta.append(math.inf)
 
@@ -59,29 +57,38 @@ def simplex_main_phase(problem: LPProblem, basis_plan: BasisPlan) -> np.ndarray 
             return None
 
         k = theta.index(theta_0)
-        basis_plan.basis[k] = j_0
-
-        for i in range(len(basis_plan.basis)):
-            if i == k:
-                continue
-            j_i = basis_plan.basis[i]
-            basis_plan.x[j_i] = basis_plan.x[j_i] - theta_0 * z[i]
+        j_asterisk = basic_solution.basis[k]
+        basic_solution.basis[k] = j_0
+        basic_solution.x[j_0] = theta_0
+        basic_solution.x[j_asterisk] = 0
+        for i in range(len(basic_solution.basis)):
+            j_i = basic_solution.basis[i]
+            if i != k:
+                basic_solution.x[j_i] = basic_solution.x[j_i] - theta_0 * z[i]
 
         print("\n--- New Basis Plan ---")
-        basis_plan.print()
+        basic_solution.print()
 
 
 def main():
-    c = [1, 1, 0, 0, 0]
-    A = [[-1, 1, 1, 0, 0], [1, 0, 0, 1, 0], [0, 1, 0, 0, 1]]
-    b = [1, 3, 2]
+    # c = [1, 1, 0, 0, 0]
+    # A = [[-1, 1, 1, 0, 0], [1, 0, 0, 1, 0], [0, 1, 0, 0, 1]]
+    # b = [1, 3, 2]
+    # ops = ['=', '=', '=']
+    # bounds = [(0, None), (0, None), (0, None), (0, None), (0, None)]
+    c = [1, 0, 0, 0]
+    A = [[1, -1, 1, 0], [-1, 1, 0, 1]]
+    b = [1, 2]
     ops = ['=', '=', '=']
     bounds = [(0, None), (0, None), (0, None), (0, None), (0, None)]
 
     lp = LPProblem(c, A, b, ops, bounds, maximize=True)
     # ZERO INDEXED
-    optimal_plan = simplex_main_phase(lp, BasisPlan([0, 0, 1, 3, 2], [2, 3, 4]))
-    print(optimal_plan)
+    optimal_plan = simplex_main_phase(lp, BasicSolution([1, 0, 0, 3], [0, 3]))
+    if optimal_plan is None:
+        print("The objective function is unbounded above on the feasible set")
+    else:
+        print(f"Optimal plan: {optimal_plan}")
 
 
 if __name__ == "__main__":
